d682b90b2fd9c5bb42c8c468840db8c97edc74a1
Seth Phat <phattranminh96@gmail.com>
2023-06-05 17:54:46 +0700

    RoadToJobs

--- server/db/migrations/00003_create_default_user.sql
+++ server/db/migrations/00003_create_default_user.sql
@@ -2,7 +2,7 @@ CREATE user
 SET
   email = 'admin@sethphat.com',
   username = 'admin',
-  password  = crypto::argon2::generate('admin')
-  name = 'Road To Jobs'
+  password  = crypto::argon2::generate('admin'),
+  name = 'Road To Jobs',
   email_verified_at = time::now()
 
