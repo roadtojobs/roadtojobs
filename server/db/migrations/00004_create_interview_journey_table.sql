d77b2ebe2e162cace0d9778339022fc3b31dc429
Seth Phat <phattranminh96@gmail.com>
2023-06-06 21:09:07 +0700

    Road To Jobs

---
+++ server/db/migrations/00004_create_interview_journey_table.sql
@@ -0,0 +1,43 @@
+DEFINE TABLE inteview_journey SCHEMAFULL
+  PERMISSIONS
+    FOR create, update, select WHERE user_id = $auth.id
+    FOR delete NONE;
+
+DEFINE FIELD user_id ON TABLE inteview_journey TYPE record (user)
+  ASSERT $value != NONE;
+
+DEFINE FIELD name ON TABLE inteview_journey
+  TYPE string
+  ASSERT $value != NONE;
+
+DEFINE FIELD description ON TABLE inteview_journey
+  TYPE string
+  ASSERT $value != NONE;
+
+DEFINE FIELD note ON TABLE inteview_journey
+  TYPE string;
+
+DEFINE FIELD started_at ON TABLE inteview_journey
+  TYPE datetime
+  VALUE $value OR time::now();
+
+DEFINE FIELD ended_at ON TABLE inteview_journey
+  TYPE datetime
+  VALUE $value OR time::now();
+
+DEFINE FIELD started_reason ON TABLE inteview_journey TYPE string;
+DEFINE FIELD ended_reason ON TABLE inteview_journey TYPE string;
+DEFINE FIELD archived_reason ON TABLE inteview_journey TYPE string;
+
+DEFINE FIELD created_at ON TABLE inteview_journey
+  TYPE datetime
+  VALUE $value OR time::now();
+
+DEFINE FIELD updated_at ON TABLE inteview_journey
+  TYPE datetime
+  VALUE time::now();
+
+DEFINE FIELD archived_at ON TABLE inteview_journey
+  TYPE datetime;
+
+
--- web/src/screens/Analytics/Analytics.vue
+++ web/src/screens/Analytics/Analytics.vue
@@ -3,9 +3,34 @@
     header-title="Analytics"
     description="Check your performance across all journeys"
   >
-    aaa
+    <dl
+      class="grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4"
+    >
+      <div
+        v-for="stat in stats"
+        :key="stat.id"
+        class="flex flex-col bg-gray-400/5 p-8"
+      >
+        <dt class="text-sm font-semibold leading-6 text-gray-600">
+          {{ stat.name }}
+        </dt>
+        <dd
+          class="order-first text-3xl font-semibold tracking-tight text-gray-900"
+        >
+          {{ stat.value }}
+        </dd>
+      </div>
+    </dl>
   </AppPage>
 </template>
+
 <script setup lang="ts">
 import AppPage from '@/components/AppPage/AppPage.vue';
+
+const stats = [
+  { id: 1, name: 'Creators on the platform', value: '8,000+' },
+  { id: 2, name: 'Flat platform fee', value: '3%' },
+  { id: 3, name: 'Uptime guarantee', value: '99.9%' },
+  { id: 4, name: 'Paid out to creators', value: '$70M' },
+];
 </script>
