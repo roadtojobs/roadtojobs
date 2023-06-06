68859273a82c6cbdaf465b25d4c33a3083f07410
Seth Phat <phattranminh96@gmail.com>
2023-06-06 17:07:02 +0700

    RoadToJobs

--- server/db/migrations/00002_create_stages_table.sql
+++ server/db/migrations/00002_create_stages_table.sql
@@ -8,3 +8,15 @@ DEFINE FIELD is_final_stage ON TABLE stage TYPE bool VALUE $value OR false;
 DEFINE FIELD created_at ON TABLE stage TYPE datetime VALUE $value OR time::now();
 DEFINE FIELD updated_at ON TABLE stage TYPE datetime VALUE time::now()
   PERMISSIONS NONE;
+
+CREATE stage:inresting CONTENT {
+  name: 'Interesting Companies',
+  is_initial_stage: true
+};
+
+CREATE stage:applied SET name = 'Applied';
+CREATE stage:ghosted SET name = 'Ghosted/No reply', is_final_stage = true;
+CREATE stage:interviewing SET name = 'Interviewing';
+CREATE stage:waiting_for_offer SET name = 'Waiting for Offer';
+CREATE stage:offered SET name = 'Offered';
+CREATE stage:accepted SET name = 'Accepted Offer', is_final_stage = true;
