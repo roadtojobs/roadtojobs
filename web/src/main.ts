48797be38875f9734919b5163a368fa3144dfb07
Seth Phat <phattranminh96@gmail.com>
2023-06-12 09:51:48 +0700

    Road To Jobs

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import './assets/css/index.postcss';
 import router from './router';
 
+const pinia = createPinia();
 const app = createApp(App);
 
 app.use(router);
+app.use(pinia);
 
 app.mount('#app');
