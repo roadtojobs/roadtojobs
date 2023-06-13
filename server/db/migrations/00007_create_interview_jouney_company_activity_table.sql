0e3ea1378b68199107d0f22101a88dbc8d394cc5
Seth Phat <phattranminh96@gmail.com>
2023-06-13 16:01:29 +0700

    Road To Jobs

---
+++ server/db/migrations/00007_create_interview_jouney_company_activity_table.sql
@@ -0,0 +1,44 @@
+DEFINE TABLE interview_journey_company_activity SCHEMAFULL
+  PERMISSIONS
+    FOR select, update, delete WHERE $auth.id = user;
+
+DEFINE FIELD interview_journey_company ON TABLE interview_journey_company_activity
+  TYPE record (interview_journey_company)
+  ASSERT $value != NONE;
+
+DEFINE FIELD user ON TABLE interview_journey_company_activity
+  TYPE record (user)
+  ASSERT $value != NONE;
+
+DEFINE FIELD type ON TABLE interview_journey_company_activity
+  TYPE string;
+
+DEFINE FIELD stage ON TABLE interview_journey_company_activity
+  TYPE record (stage);
+
+DEFINE FIELD comment ON TABLE interview_journey_company_activity
+  TYPE string;
+
+DEFINE FIELD attributes ON TABLE interview_journey_company_activity
+  TYPE array;
+
+DEFINE FIELD attributes.* ON TABLE interview_journey_company_activity
+  TYPE object;
+
+DEFINE FIELD attributes.*.text ON TABLE interview_journey_company_activity
+  TYPE string;
+
+DEFINE FIELD attributes.*.color ON TABLE interview_journey_company_activity
+  TYPE string;
+
+DEFINE FIELD created_at ON TABLE interview_journey_company_activity
+  TYPE datetime
+  VALUE $value OR time::now()
+  PERMISSIONS
+    FOR update, delete NONE;
+
+DEFINE FIELD updated_at ON TABLE interview_journey_company_activity
+  TYPE datetime
+  VALUE time::now()
+  PERMISSIONS
+    FOR update, delete NONE;
---
+++ shared/src/constants/activityType.ts
@@ -0,0 +1,5 @@
+export type ActivityType =
+  | 'ADDED_NOTE'
+  | 'ADDED_ATTRIBUTE'
+  | 'REMOVED_ATTRIBUTE'
+  | 'ADVANCED_TO_STAGE';
