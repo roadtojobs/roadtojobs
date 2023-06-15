5930e80995b791d5657b926d4eb2342abcc3a6e8
Seth Phat <phattranminh96@gmail.com>
2023-06-15 23:08:40 +0700

    Road To Jobs

---
+++ web/src/components/ConfirmationModal/ConfirmationModal.vue
@@ -0,0 +1,30 @@
+<template>
+  <Modal
+    :is-open="isOpen"
+    @close="$emit('close')"
+    :title="title"
+  >
+    <div class="text-sm"><slot /></div>
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import Modal from '@/components/Modal/Modal.vue';
+import { withDefaults } from 'vue';
+
+type ConfirmationModalProps = {
+  isOpen: boolean;
+  title?: string;
+};
+
+type ConfirmationModalEmits = {
+  (e: 'close'): void;
+};
+
+withDefaults(defineProps<ConfirmationModalProps>(), {
+  isOpen: false,
+  title: 'Confirm the Action',
+});
+
+defineEmits<ConfirmationModalEmits>();
+</script>
