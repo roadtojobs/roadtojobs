bbb250780be2103cff7fa06952f18d266724201f
Seth Phat <phattranminh96@gmail.com>
2023-07-28 19:57:50 +0700

    Added new stage

---
+++ server/db/migrations/000013_add_stopped_stage.sql
@@ -0,0 +1,8 @@
+CREATE stage:stopped CONTENT {
+  name: 'Stopped ❌',
+  description: 'Companies that you prevented from progressing (due to various factors, including their actions or your decisions,...)',
+  is_final_stage: true,
+  is_bad_stage: true,
+  color: 'red',
+  position: 65
+};
